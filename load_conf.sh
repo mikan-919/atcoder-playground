605246279acb923d085936d03060b0a1361aab0f
mikan-919 <131849133+mikan-919@users.noreply.github.com>
2024-07-10 23:56:33 +0900

    改善

--- load_conf.sh
+++ load_conf.sh
@@ -1,10 +1,11 @@
 dir=`dirname $0`
-ORIGIN_ROOT=$(realpath $dir)
+ORIGIN_ROOT=$(realpath $dir/config)
 CONFIG_ROOT=$(realpath ~/.config/atcoder-cli-nodejs)
 
 function def(){
-  ln -s $ORIGIN_ROOT/$1 $CONFIG_ROOT/$1
+  cp -s $ORIGIN_ROOT/$1 $CONFIG_ROOT/$1
 }
+rm -rf ~/.config/atcoder-cli-nodejs/
 mkdir -p ~/.config/atcoder-cli-nodejs/python
 def config.json
 def python/main.py
