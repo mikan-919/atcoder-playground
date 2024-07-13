da3e98b62f3447e163923fb1813f6219594064d2
mikan-919 <131849133+mikan-919@users.noreply.github.com>
2024-07-13 17:34:47 +0900

    abc103/b

--- abc103/b/main.py
+++ abc103/b/main.py
@@ -10,3 +10,16 @@ def input_strs()->list[str]:
   return input().split()
 
 ### == == == == == Pre Lib == == == == == ###
+
+def main(S,T):
+  length=len(S)
+  S=list(S)
+  for i in range(length):
+    if "".join(S) ==T:
+      return "Yes"
+    S.insert(0,S[-1]) 
+    S.pop(-1)
+  return "No"
+
+result=main(input_str(),input_str())
+print(result)
