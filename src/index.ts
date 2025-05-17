fe23a6fe17bf04f2c61e2a695da00a6b02834f94
摘果みかん <131849133+mikan-919@users.noreply.github.com>
2025-05-17 11:58:46 +0000

    base

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,5 @@ import { no, put, stop, yes } from './stdout'
 import { edgeListToAdjacencyList } from './utils/graph'
 import { range } from './utils/utils'
 
-const [N] = readNumbers()
-const A = readNumbers().map(BigInt)
-
-A.reverse()
-
-let cumulativeSum = 0n
-const C = A.map((num) => (cumulativeSum += num)).reverse()
-
-A.reverse()
-
-let sum = 0n
-for (const i of range(N)) {
-  sum += (A[i - 1] ?? 0n) * C[i]
-}
-
-put(sum.toString())
+// const [N] = readNumbers()
+// const [A,B] = readNumbers()
