a06ffd68f84d7e7df735de0f247fd01bb8332812
摘果みかん <131849133+mikan-919@users.noreply.github.com>
2025-05-17 12:29:43 +0000

    abc-406-c

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,24 @@ import { no, put, stop, yes } from './stdout'
 import { edgeListToAdjacencyList } from './utils/graph'
 import { range } from './utils/utils'
 
-const [N, K] = readNumbers()
-const A = readNumbers()
-let counter = 1
-for (const ai of A) {
-  counter *= ai
-  if (counter.toString().length>=K+1) counter = 1
+const [N] = readNumbers()
+const P = readNumbers()
+
+const j4 = P.map((e, i) => (i === 0 ? false : P[i - 1] < e && e > P[i + 1])) // O(n)
+const j5 = P.map((e, i) => (i === 0 ? false : P[i - 1] > e && e < P[i + 1])) // O(n)
+let j4imosc = 0
+let j5imosc = 0
+const j4imos = j4.map((num) => (j4imosc += num)) // O(n)
+const j5imos = j4.map((num) => (j5imosc += num)) // O(n)
+
+let counter = 0
+// O(n*k)
+for (const i of range(N - 4)) {
+  if (!(P[i] < P[i + 1])) continue
+  let scope = 4
+  while (j4imos[i + scope] - j4imos[i] === 1 && j5imos[i + scope] - j5imos[i] === 1 && i + scope < P.length) {
+    counter++
+    scope++
+  }
 }
 put(counter)
