96c7056ba90014e3075d0fa3334ec3f865a97a8b
摘果みかん <131849133+mikan-919@users.noreply.github.com>
2025-07-19 12:01:54 +0000

    ABC415-a

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,8 @@ import { readLine, readLines, readMatrix, readNumberLines, readNumbers } from '.
 import { no, put, putArray, yes } from './stdout'
 import { range } from './utils/utils'
 
-const Q = readNumbers()
-const queries = readNumberLines(Q[0])
-const A: [number, number][] = []
-for (const query of queries) {
-  const [type, c, x] = query
-  if (type === 1) {
-    A.push([x, c])
-  } else {
-    let k = 0
-    let i = 0
-    let ans = 0
-    while (k !== c) {
-      put(k)
-      if (A[i][1] === 0) {
-        i++
+const [N]=readNumbers()
+const A=readNumbers()
+const [X]=readNumbers()
 
-        continue
-      }
-      if (A[i][1] >= c - k) {
-        k += A[i][1]
-        ans += A[i][1] * A[i][0]
-        A[i][1] = 0
-      } else {
-        A[i][1] -= c - k
-        ans += A[i][0] * (c - k)
-        k = c
-      }
-      i++
-    }
-  }
-}
+A.includes(X)?yes():no()
